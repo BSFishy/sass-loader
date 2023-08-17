7bf2062892be738ff7d529683be891a5dd400991
Matt Provost <provomat@amazon.com>
2023-08-17 00:39:12 +0000

    feat: add sass-embedded to auto loading
    Signed-off-by: Matt Provost <provomat@amazon.com>

--- src/utils.js
+++ src/utils.js
@@ -15,8 +15,13 @@ function getDefaultSassImplementation() {
     try {
       require.resolve("node-sass");
       sassImplPkg = "node-sass";
-    } catch (ignoreError) {
-      sassImplPkg = "sass";
+    } catch (error) {
+      try {
+        require.resolve("sass-embedded");
+        sassImplPkg = "sass-embedded";
+      } catch (ignoreError) {
+        sassImplPkg = "sass";
+      }
     }
   }
 
@@ -241,8 +246,7 @@ async function getSassOptions(
 // - ~@org/
 // - ~@org/package
 // - ~@org/package/
-const isModuleImport =
-  /^~([^/]+|[^/]+\/|@[^/]+[/][^/]+|@[^/]+\/?|@[^/]+[/][^/]+\/)$/;
+const isModuleImport = /^~([^/]+|[^/]+\/|@[^/]+[/][^/]+|@[^/]+\/?|@[^/]+[/][^/]+\/)$/;
 
 /**
  * When `sass`/`node-sass` tries to resolve an import, it uses a special algorithm.
